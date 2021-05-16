557c84bc8a88c1d4268af1220c747d6d40a4daa0
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-05-17 01:17:30 +0300

    last commit

---
+++ DreamTeamProject.ViewModels/AddCommnetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DreamTeamProject.ViewModels
+{
+    public class AddCommnetViewModel
+    {
+        public string Context { get; set; }
+        public int UserId { get;set; }
+        public int BookId { get; set; }
+    }
+}
---
+++ DreamTeamProject.ViewModels/GetBookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DreamTeamProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DreamTeamProject.ViewModels
+{
+    public class GetBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
